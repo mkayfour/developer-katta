f3971fe0e3da7fba1338a61a98ffeefd68e32519
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-27 15:42:33 +0530

    added routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 const app = express();
 
@@ -15,3 +19,9 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('API Running')
 });
+
+// Define routers
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);

beac006a465de7133ff66b9387e881892c40ee50
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-27 15:29:50 +0530

    added mongo connection

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require ('express');
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// connect db
+connectDB();
+
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
 });
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('API Running')
 });

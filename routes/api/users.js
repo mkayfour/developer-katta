0f763bc8abbe48d6d5f5d86749567081dcd9d91f
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-27 15:59:05 +0530

    added register user email and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/auth
-// @desc Test route
+// @route POST api/users
+// @desc Register user
 // @access Public
-router.get('/', (req, res) => { res.send('User route') });
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid e-mail').isEmail(),
+    check('password',
+        'Please enter a valid password of 6 or more characters').isLength({ min: 7 })
+],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        console.log(req.body);
+        res.send('User ROute');
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 5000;
 // connect db
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
 });

f023a0abe192061c732c855e0b65ee69e522a530
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-28 12:39:51 +0530

    added code for add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const auth = require("../../middleware/auth");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route GET api/post
-// @desc Test route
-// @access Public
-router.get("/", (req, res) => {
-  res.send("Posts route");
-});
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route POST api/post
+// @desc create a post
+// @access Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      return res.json({ msg: "server error" });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const users = require('./routes/api/users');
-const auth = require('./routes/api/auth');
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
+const express = require("express");
+const connectDB = require("./config/db");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 
 const app = express();
 
@@ -16,15 +16,15 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`)
+  console.log(`Server started on port: ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('API Running')
+app.get("/", (req, res) => {
+  res.send("API Running");
 });
 
 // Define routers
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
+app.use("/api/post", posts);

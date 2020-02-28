cabdc50e89ea3b6e5fe64d165b271e52662c53e1
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-28 16:14:03 +0530

    implemented login form frontend

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    console.log("Form Data Success");
+  };
+
+  const { email, password } = formData;
+
+  return (
+    <Fragment>
+      <section class="container">
+        <h1 class="large text-primary">Sign In</h1>
+        <p class="lead">
+          <i class="fas fa-user"></i> Sign In your account
+        </p>
+        <form class="form" onSubmit={e => onSubmit(e)}>
+          <div class="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+            <small class="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+
+          <input type="submit" class="btn btn-primary" value="Login" />
+        </form>
+        <p class="my-1">
+          Don't have an account? <Link to="register"> Sign Up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,12 +12,12 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log("Form Data", formData);
+      console.log("Form Data Success");
     }
   };
 
@@ -79,7 +80,7 @@ const Register = () => {
           <input type="submit" class="btn btn-primary" value="Register" />
         </form>
         <p class="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
+          Already have an account?<Link to="login"> Sign In</Link>
         </p>
       </section>
     </Fragment>

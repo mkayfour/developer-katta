e0b70a610cd84b9c21a767fdc3b16ce2d31c76db
Mohit K <mohit.kumar@zconsolutions.com>
2020-02-28 15:44:49 +0530

    setup basic react code

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+        <Landing />
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
